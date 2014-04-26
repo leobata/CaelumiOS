e066bb74e2ca69b97b94582c5a173859d8d04fc4
leobata <leobata@gmail.com>
2014-04-26 16:17:15 -0300

    WebServices working

--- WebServices/WebServices/AppDelegate.m
+++ WebServices/WebServices/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @implementation AppDelegate
 
@@ -16,6 +17,10 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    ViewController *vc = [[ViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+    self.window.rootViewController = nav;
     return YES;
 }
 
