128173f6dac3304c364bf3840da597bf6d08ca36
leobata <leobata@gmail.com>
2014-04-26 14:53:46 -0300

    WebServices Project

---
+++ WebServices/WebServices/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebServices
+//
+//  Created by ios4341 on 26/04/14.
+//  Copyright (c) 2014 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
